a8f56202dfe9aefd3d73f11977730838c63a9c10
Abdoula Cherif <abdoula13cherif@gmail.com>
2026-05-31 22:25:07 +0100

    Ajout PWA manifest et service worker

---
+++ app/static/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'boostcentral-v1';
+const urlsToCache = ['/', '/auth/login', '/static/manifest.json'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
